f7e47b9425995349a3783d0e7cadf47246802019
Katherine James <kajames@lincoln.ac.uk>
2023-08-11 15:42:33 +0100

    Match poly and confusion matrix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from scipy.optimize import linear_sum_assignment
 
 # general utils
 def distance(nodes, edges):
@@ -7,6 +8,12 @@ def distance(nodes, edges):
     p0 = nodes[e0]
     p1 = nodes[e1]
     return math.dist(p0, p1)
+
+def dot_product(v1, v2):
+    return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]
+
+def magnitude(vector):
+    return math.sqrt(sum(pow(element, 2) for element in vector))
 
 # graph utils
 def create_adjacency_matrix(edges, nodes):
@@ -31,7 +38,39 @@ def edges_from_matrix(adj_matrix):
                 new_edges.append([row,col])
     return new_edges
 
+def find_branch_nodes(graph):
+    branch_nodes = []
+    for node in graph:
+        if len(graph[node]) > 2:
+            branch_nodes.append(node)
+    return branch_nodes
 
+def create_graph_from_adjacency_matrix(adj_matrix):
+    graph = {}
+    num_nodes = adj_matrix.shape[0]
+
+    for i in range(num_nodes):
+        neighbors = []
+        for j in range(num_nodes):
+            if adj_matrix[i, j] > 0:
+                neighbors.append(j)
+        graph[i] = neighbors
+
+    return graph
+
+def create_adjacency_matrix(edges, nodes):
+    # Initialize an empty adjacency matrix
+    num_vertices = len(nodes)
+    adj_matrix = np.zeros((num_vertices, num_vertices), dtype=float)
+
+    # Populate the adjacency matrix based on the edges
+    for edge in edges:
+        src, dest = edge
+        d = np.linalg.norm(nodes[src]-nodes[dest])               
+        adj_matrix[src][dest] = d
+        adj_matrix[dest][src] = d  # If the graph is undirected, add this line as well
+
+    return adj_matrix
 
 # functions
 
@@ -112,4 +151,177 @@ def create_dense(nodes, edges, s = 0.5, return_adj=False):
 
     return np.array(dense_nodes), np.array(dense_edges)
 
-# ---
+# --- match polyline
+
+def point_near_line_segment(P,A,B,t):
+    x, y, z = P
+    x1, y1, z1 = A
+    x2, y2, z2 = B
+
+    # Calculate the vectors
+    vector_AB = (x2 - x1, y2 - y1, z2 - z1)
+    vector_AC = (x - x1, y - y1, z - z1)
+
+    # Calculate the projection P onto vector AB
+    dot_AB_AB = dot_product(vector_AB, vector_AB)
+    dot_AC_AB = dot_product(vector_AC, vector_AB)
+    scalar = dot_AC_AB / dot_AB_AB
+    projection_P = (vector_AB[0] * scalar, vector_AB[1] * scalar, vector_AB[2] * scalar)
+
+    # Calculate the vector from C to P
+    vector_CP = (vector_AC[0] - projection_P[0], vector_AC[1] - projection_P[1], vector_AC[2] - projection_P[2])
+
+    # Calculate the distance from C to P
+    distance_CP = magnitude(vector_CP)
+
+    return distance_CP <= t
+    
+def find_contiguous(graph, start_node, true_positive_nodes, visited):
+    section = set()  # To store the current contiguous section
+    stack = [start_node]
+    
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.add(node)
+            section.add(node)
+            stack.extend(neighbor for neighbor in graph.get(node, [])
+                         if neighbor in true_positive_nodes)
+    
+    return list(section)
+
+def find_contiguous_sections(graph, nodes):
+    visited = set()  # To keep track of visited nodes
+    contiguous_sections = []
+
+    for node in nodes:
+        if node not in visited:
+            section = find_contiguous(graph, node, nodes, visited)            
+            contiguous_sections.append(section)    
+    
+    return contiguous_sections
+
+def split_into_branches(graph):
+    branch_nodes = find_branch_nodes(graph)
+    branches = None
+    for this_branch in branch_nodes:  
+        connections = graph[this_branch] #  follow each connection until it is a leaf or a branch node
+        
+        branches = []
+        visited = set([])
+
+        def branch_dfs(node, branch):
+            visited.add(node)
+            branch.append(node)
+            for neighbor in graph[node]:
+                if neighbor not in visited and neighbor not in branch_nodes:
+                    branch_dfs(neighbor, branch)
+                if neighbor in branch_nodes:
+                    branch.append(neighbor)
+
+        for node in connections:
+            branch = []
+            branch_dfs(node, branch)              
+            branches.append(branch)
+
+    # if no branches
+    if branches:
+        # order branches
+        sorted_branches = []
+        for branch in branches:
+            c = find_contiguous_sections(graph, branch)
+            sorted_branches.append(c[0])
+    
+        return sorted_branches
+    else:
+        section = find_contiguous_sections(graph, list(graph.keys()))      
+        return section
+
+def match_polyline_graphs(graph1, graph2, nodes1, nodes2, thresh, line_dist_thresh = 0.25):
+    """
+    Matches graph2 against graph 1 (GT)
+    Returns a dictionary of the matched indices between the two graphs
+    """
+    match_dict = {}
+    keys = graph1.keys()   
+
+    # find lines between the nodes in the gt
+    line_segments = [] 
+     
+    contiguous = split_into_branches(graph1)
+    
+    # print(contiguous)
+    for i in range(0,len(contiguous)):  
+        contig = contiguous[i]
+       
+        for p in range(0,len(contig)-1):
+            index_i = contig[p]
+            index_i1 = contig[p+1]            
+
+            A = nodes1[index_i]
+            B = nodes1[index_i1]
+
+            line_segments.append((A,B))           
+    
+    g2_tp = []
+    for i in list(graph2.keys()):
+        for A,B in line_segments:
+            if point_near_line_segment(nodes2[i], A, B, line_dist_thresh): # distance from polyline
+                g2_tp.append(i)
+
+    cost_matrix = np.ones((len(graph1.keys()), len(graph2.keys()))) * 1000
+    for k1 in graph1.keys():        
+        for k2 in graph2.keys():         
+            d = np.linalg.norm(nodes1[k1] - nodes2[k2])            
+            if d < thresh:
+                cost_matrix[k1][k2] = d         
+                
+    row_ind, col_ind = linear_sum_assignment(cost_matrix, maximize=False) # what if there are unmatched ones, they need to come out as -1
+          
+    for i in zip(row_ind,col_ind):
+        k,v = i
+        match_dict[k] = v 
+        
+    for k1 in graph1.keys():        
+        if len(np.unique(cost_matrix[k1,:])) == 1 or k1 not in match_dict.keys():
+            # unmatched
+            match_dict[k1] = -1 
+            
+    for k in keys:
+        if k not in match_dict.keys():
+            print(k)
+    
+              
+    return match_dict, list(set(g2_tp))
+
+# --- confusion matrix
+
+def corresponding_tp(match_dict):  
+    g2_matches = np.array([match_dict[k] for k in match_dict.keys()])    
+    g2_matches = g2_matches[g2_matches != -1]
+
+    return g2_matches
+
+def find_unmatched(match_dict, graph2):
+    
+    g2_matches = corresponding_tp(match_dict) 
+    g2_unmatched = graph2.keys()- g2_matches           
+        
+    return list(g2_unmatched)
+
+def confusion_matrix(match_dict, g2): 
+    """NB: If you are using match_polyline_graph then the fp value here
+    might contain some tp so should recalcuate as fp = list(set(g2.keys()) - set(tp_e))
+    where tp_e are returned by match_polyline_graph"""  
+    tp = []
+    fn = []
+ 
+    for k in match_dict.keys():
+        if match_dict[k] != -1:
+            tp.append(k)
+        else:
+            fn.append(k)
+
+    fp = find_unmatched(match_dict, g2)
+   
+    return tp,fn, fp
--- test_spec.py
+++ test_spec.py
@@ -1,6 +1,8 @@
 import numpy as np
-from main import create_dense
+from main import create_graph_from_adjacency_matrix, create_adjacency_matrix
+from main import create_dense, match_polyline_graphs, confusion_matrix, corresponding_tp
 
+# test dense 
 def test_create_dense():
     a = [0,0,0]
     b = [2,0,0]
@@ -59,6 +61,419 @@ def test_create_dense2():
                                         [5, 6],
                                         [7, 8],
                                         [8, 9],])
+
+# test match
+
+def test_match_graphs():
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [6, 5, 4]
+    nodes = np.array([node0, node1, node2, node3, node4])
+    edges = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 1, 0, 0, 0],
+            [1, 0, 3, 0, 5],
+            [0, 3, 0, 6, 0],
+            [0, 0, 6, 0, 0],
+            [0, 5, 0, 0, 0],
+        ]
+    )
+    g = create_graph_from_adjacency_matrix(adj_matrix)
+    match_dict,_ = match_polyline_graphs(g, g, nodes, nodes, 1)
+
+    assert match_dict[0] == 0
+    assert match_dict[1] == 1
+    assert match_dict[2] == 2
+    assert match_dict[3] == 3
+    assert match_dict[4] == 4
+
+
+def test_match_graphs2():
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [6, 5, 4]
+    nodes1 = np.array([node0, node1, node2, node3, node4])
+    edges1 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 1, 0, 0, 0],
+            [1, 0, 3, 0, 5],
+            [0, 3, 0, 6, 0],
+            [0, 0, 6, 0, 0],
+            [0, 5, 0, 0, 0],
+        ]
+    )
+
+    g1 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [20, 20, 20]
+    nodes2 = np.array([node0, node1, node2, node3, node4])
+    edges2 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 1, 0, 0, 0],
+            [1, 0, 3, 0, 5],
+            [0, 3, 0, 6, 0],
+            [0, 0, 6, 0, 0],
+            [0, 5, 0, 0, 0],
+        ]
+    )
+
+    g2 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    match_dict,_ = match_polyline_graphs(g1, g2, nodes1, nodes2, 1)
+
+    assert match_dict[0] == 0
+    assert match_dict[1] == 1
+    assert match_dict[2] == 2
+    assert match_dict[3] == 3
+    assert match_dict[4] == -1
+
+def test_match_reversed():
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [6, 5, 4]
+    nodes1 = np.array([node0, node1, node2, node3, node4])
+    edges1 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 1, 0, 0, 0],
+            [1, 0, 3, 0, 5],
+            [0, 3, 0, 6, 0],
+            [0, 0, 6, 0, 0],
+            [0, 5, 0, 0, 0],
+        ]
+    )
+
+    g1 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [20, 20, 20]
+    node5 = [20, 25, 20]
+    nodes2 = np.array([node5, node1, node2, node3, node4, node0])
+    edges2 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 0, 0, 0, 0, 1],
+            [0, 0, 3, 0, 5, 1],
+            [0, 3, 0, 6, 0, 0],
+            [0, 0, 6, 0, 0, 0],
+            [0, 5, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0],
+        ]
+    )
+
+    g2 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    match_dict,_ = match_polyline_graphs(g1, g2, nodes1, nodes2, 1)
+
+    assert match_dict[0] == 5, match_dict[0]
+    assert match_dict[1] == 1, match_dict[1]
+    assert match_dict[2] == 2, match_dict[2]
+    assert match_dict[3] == 3, match_dict[3]
+    assert match_dict[4] == -1, match_dict[4]
+
+def test_match_graphs_diff_density():
+    node0 = [0,0,1]
+    node1 = [0,0,2]
+    node2 = [0,0,3] # branch
+    node3 = [0,0,4]
+    node4 = [0,1,3]
+    nodes1 = np.array([node0, node1, node2, node3, node4])
+    edges1 = np.array([[0, 1], [1, 2], [2, 3], [4, 2]])
+
+    adj_matrix = create_adjacency_matrix(edges1,nodes1)
+
+    g1 = create_graph_from_adjacency_matrix(adj_matrix)
+    
+    node0 = [0,0,1]   
+    node00 = [0,0,1.5] 
+    node1 = [0,0,2]
+    node10 = [0,0,2.5]
+    node2 = [0,0,3] # 4- branch
+    node3 = [0,0,4]
+    node4 = [0,1,3] 
+    node5 = [0,0.3,1.5]  
+    node6 = [0,0.5,3]
+    nodes2 = np.array([node0,node00,node1,node10,node2,node3,node4,node5,node6])
+    edges2 = np.array([[0, 1], [1, 2], [2, 3], [3,4], [4,5], [4,7], [7,8], [8,6]])
+
+    adj_matrix = create_adjacency_matrix(edges2, nodes2)
+
+    g2 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    match_dict, tp_e = match_polyline_graphs(g1, g2, nodes1, nodes2, 1)   
+
+    assert np.array_equal(tp_e, [0, 1, 2, 3, 4, 5, 6, 8]) 
+
+#  --- confusion matrix
+def test_match_graphs_diff_density():
+    node0 = [0,0,1]
+    node1 = [0,0,2]
+    node2 = [0,0,3] # branch
+    node3 = [0,0,4]
+    node4 = [0,1,3]
+    nodes1 = np.array([node0, node1, node2, node3, node4])
+    edges1 = np.array([[0, 1], [1, 2], [2, 3], [4, 2]])
+
+    adj_matrix = create_adjacency_matrix(edges1,nodes1)
+
+    g1 = create_graph_from_adjacency_matrix(adj_matrix)
+    
+    node0 = [0,0,1]   
+    node00 = [0,0,1.5] 
+    node1 = [0,0,2]
+    node10 = [0,0,2.5]
+    node2 = [0,0,3] # 4- branch
+    node3 = [0,0,4]
+    node4 = [0,1,3] 
+    node5 = [0,0.3,1.5]  
+    node6 = [0,0.5,3]
+    nodes2 = np.array([node0,node00,node1,node10,node2,node3,node4,node5,node6])
+    edges2 = np.array([[0, 1], [1, 2], [2, 3], [3,4], [4,5], [4,7], [7,8], [8,6]])
+
+    adj_matrix = create_adjacency_matrix(edges2, nodes2)
+
+    g2 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    match_dict, tp_e = match_polyline_graphs(g1, g2, nodes1, nodes2, 1)   
+
+    assert np.array_equal(tp_e, [0, 1, 2, 3, 4, 5, 6, 8]) 
+
+    tp, fn, _ = confusion_matrix(match_dict, g2) #
+    fp = list(set(g2.keys()) - set(tp_e))
+    tp_e = corresponding_tp(match_dict) 
+
+    false_positives = nodes2[fp]
+    true_positives_e = nodes2[tp_e] 
+    true_positives = nodes1[tp]
+    false_negatives = nodes1[fn]    
+
+    assert len(false_positives) == 1, false_positives
+    assert np.array_equal(false_positives[0],[0,0.3,1.5]), false_positives
+
+def test_confusion_matrix_reversed():
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [6, 5, 4]
+    nodes1 = np.array([node0, node1, node2, node3, node4])
+    edges1 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 1, 0, 0, 0],
+            [1, 0, 3, 0, 5],
+            [0, 3, 0, 6, 0],
+            [0, 0, 6, 0, 0],
+            [0, 5, 0, 0, 0],
+        ]
+    )
+
+    g1 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [20, 20, 20]
+    node5 = [20, 25, 20]
+    nodes2 = np.array([node5, node1, node2, node3, node4, node0])
+    edges2 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 0, 0, 0, 0, 1],
+            [0, 0, 3, 0, 5, 1],
+            [0, 3, 0, 6, 0, 0],
+            [0, 0, 6, 0, 0, 0],
+            [0, 5, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0],
+        ]
+    )
+
+    g2 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    match_dict,_ = match_polyline_graphs(g1, g2, nodes1, nodes2, 1)
+
+    tp, fn, fp = confusion_matrix(match_dict, g2)
+
+    assert np.array_equal(tp, [0, 1, 2, 3]), tp  # indicies in g1
+    assert np.array_equal(fn, [4]), fn  # indicies in g1
+    assert np.array_equal(fp, [0, 4]), fp  # indicies in g2
+
+def test_confusion_matrix():
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [6, 5, 4]
+    nodes1 = np.array([node0, node1, node2, node3, node4])
+    edges1 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 1, 0, 0, 0],
+            [1, 0, 3, 0, 5],
+            [0, 3, 0, 6, 0],
+            [0, 0, 6, 0, 0],
+            [0, 5, 0, 0, 0],
+        ]
+    )
+
+    g1 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [20, 20, 20]
+    nodes2 = np.array([node0, node1, node2, node3, node4])
+    edges2 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 1, 0, 0, 0],
+            [1, 0, 3, 0, 5],
+            [0, 3, 0, 6, 0],
+            [0, 0, 6, 0, 0],
+            [0, 5, 0, 0, 0],
+        ]
+    )
+
+    g2 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    match_dict,_ = match_polyline_graphs(g1, g2, nodes1, nodes2, 1)
+    tp, fn, fp = confusion_matrix(match_dict, g2)
+
+    assert np.array_equal(tp, [0, 1, 2, 3])
+    assert np.array_equal(fn, [4])
+    assert np.array_equal(fp, [4])
+
+    assert len(tp) + len(fn) == len(g1)
+    assert len(tp) + len(fp) == len(g2)
+
+def test_confusion_matrix2():
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [6, 5, 4]
+    nodes1 = np.array([node0, node1, node2, node3, node4])
+    edges1 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 1, 0, 0, 0],
+            [1, 0, 3, 0, 5],
+            [0, 3, 0, 6, 0],
+            [0, 0, 6, 0, 0],
+            [0, 5, 0, 0, 0],
+        ]
+    )
+
+    g1 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [20, 20, 20]
+    node5 = [20, 25, 20]
+    nodes2 = np.array([node0, node1, node2, node3, node4, node5])
+    edges2 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 1, 0, 0, 0, 1],
+            [1, 0, 3, 0, 5, 0],
+            [0, 3, 0, 6, 0, 0],
+            [0, 0, 6, 0, 0, 0],
+            [0, 5, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0],
+        ]
+    )
+
+    g2 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    match_dict,_ = match_polyline_graphs(g1, g2, nodes1, nodes2, 1)
+    tp, fn, fp = confusion_matrix(match_dict, g2)
+
+    assert np.array_equal(tp, [0, 1, 2, 3])
+    assert np.array_equal(fn, [4])
+    assert np.array_equal(fp, [4, 5])
+
+    assert len(tp) + len(fn) == len(g1)
+    assert len(tp) + len(fp) == len(g2)
+
+def test_confusion_matrix3():
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    nodes1 = np.array([node0, node1])
+    edges1 = np.array([[0, 1], [1, 2], [2, 3], [4, 1]])
+
+    adj_matrix = np.array(
+        [
+            [0, 1],
+            [1, 0],
+        ]
+    )
+
+    g1 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    node0 = [1, 2, 3]
+    node1 = [4, 5, 6]
+    node2 = [6, 7, 8]
+    node3 = [8, 10, 11]
+    node4 = [20, 20, 20]
+    node5 = [20, 25, 20]
+    nodes2 = np.array([node0, node1, node2, node3, node4, node5])
+
+    adj_matrix = np.array(
+        [
+            [0, 1, 0, 0, 0, 1],
+            [1, 0, 3, 0, 5, 0],
+            [0, 3, 0, 6, 0, 0],
+            [0, 0, 6, 0, 0, 0],
+            [0, 5, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0],
+        ]
+    )
+
+    g2 = create_graph_from_adjacency_matrix(adj_matrix)
+
+    match_dict,_ = match_polyline_graphs(g1, g2, nodes1, nodes2, 1)
+    tp, fn, fp = confusion_matrix(match_dict, g2)
+
+    assert np.array_equal(tp, [0, 1])
+    assert np.array_equal(fn, [])
+    assert np.array_equal(fp, [2, 3, 4, 5])
+
+    assert len(tp) + len(fn) == len(g1)
+    assert len(tp) + len(fp) == len(g2)
+
+
+
 
 
 
